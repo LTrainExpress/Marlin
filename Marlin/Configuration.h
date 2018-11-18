24ecea9cfbabe6354c5622feb2a7b3767402e796
you <you@example.com>
2018-11-18 13:28:58 -0500

    Fixed feed rate

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -611,7 +611,7 @@
  */
 //#define DEFAULT_MAX_FEEDRATE          { 4800, 4800, 1000, 250} // LRW Modifed for CR-10s
 //#define DEFAULT_MAX_FEEDRATE          { 500, 500, 5, 30, 30} // LRW Modifed for CR-10s
-#define DEFAULT_MAX_FEEDRATE          { 4800, 4800, 1000, 250, 250} // LRW Modifed for CR-10s
+#define DEFAULT_MAX_FEEDRATE          { 300, 300, 5, 25, 25} // LRW Modifed for CR-10s
 
 /**
  * Default Max Acceleration (change/s) change = mm/s
@@ -619,7 +619,7 @@
  * Override with M201
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-#define DEFAULT_MAX_ACCELERATION      { 3000, 3000, 100, 10000}
+#define DEFAULT_MAX_ACCELERATION      { 3000, 3000, 100, 5000}
 
 /**
  * Default Acceleration (change/s) change = mm/s
@@ -629,9 +629,9 @@
  *   M204 R    Retract Acceleration
  *   M204 T    Travel Acceleration
  */
-#define DEFAULT_ACCELERATION          2000    // X, Y, Z and E acceleration for printing moves
-#define DEFAULT_RETRACT_ACCELERATION  3000    // E acceleration for retracts
-#define DEFAULT_TRAVEL_ACCELERATION   2000    // X, Y, Z acceleration for travel (non printing) moves
+#define DEFAULT_ACCELERATION          500    // X, Y, Z and E acceleration for printing moves
+#define DEFAULT_RETRACT_ACCELERATION  500    // E acceleration for retracts
+#define DEFAULT_TRAVEL_ACCELERATION   1000    // X, Y, Z acceleration for travel (non printing) moves
 
 /**
  * Default Jerk (mm/s)
@@ -644,7 +644,7 @@
 #define DEFAULT_XJERK                 4.0
 #define DEFAULT_YJERK                 4.0
 #define DEFAULT_ZJERK                  0.3
-#define DEFAULT_EJERK                  5.0
+#define DEFAULT_EJERK                  3.0
 
 /**
  * S-Curve Acceleration
